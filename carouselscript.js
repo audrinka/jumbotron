a7de8a73d4685664ab45d1854d8ecce54d94fdc6
audrinka <agnieszkaklosik@hotmail.com>
2016-08-17 18:02:04 +0200

    Update carouselscript.js

--- carouselscript.js
+++ carouselscript.js
@@ -7,10 +7,9 @@ $(function(){
 	}
 });
 
-$ function (moveFirstSlide {
+$ function moveFirstSlide() {
 	var firstItem = carouselList.find("li:first");
 	var lastItem = carouselList.find("li:last");
 	lastItem.after(firstItem);
 	carouselList.css({marginLeft:0});
-	}
-)
+};
